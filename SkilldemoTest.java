d2bbb2fcbee451d476d41c28759b7a330fb699fd
Leica <jis033@ucsd.edu>
2022-02-03 13:09:45 -0800

    fail

---
+++ SkilldemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkilldemoTest {
+    @Test
+    public void addition() {
+        assertEquals(2, Skilldemo.sum(1, 2));
+    }
+}
