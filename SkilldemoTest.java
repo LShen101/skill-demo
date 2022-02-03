d2da83338f192f5dba16d415c65377201780751e
Leica <jis033@ucsd.edu>
2022-02-03 13:11:02 -0800

    fixed

--- SkilldemoTest.java
+++ SkilldemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkilldemoTest {
     @Test
     public void addition() {
-        assertEquals(2, Skilldemo.sum(1, 2));
+        assertEquals(2, Skilldemo.sum(1, 1));
     }
 }
